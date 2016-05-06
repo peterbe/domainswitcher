ad8bb288a227d2fa4554e3d161315f4cd0c4b5ef
Peter Bengtsson <mail@peterbe.com>
2016-05-06 17:12:04 -0400

    use pageAction instead

--- choose_domain.js
+++ choose_domain.js
@@ -1,86 +1,105 @@
+var urlparse = function(url) {
+  var parser = document.createElement('a')
+  parser.href = url
+  return parser
+}
+
 var submit = function(destination) {
   if (!destination.trim().length) {
-    return;
+    return
   }
-  animateDots();
-  chrome.tabs.executeScript(null, {
-    file: "/content_scripts/domainswitch.js"
-  });
+  animateDots()
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {destination: destination});
-  });
-};
+    chrome.pageAction.hide(tabs[0].id)
+
+    var origin = urlparse(tabs[0].url)
+    var goto = urlparse(destination)
+    var newDestination = goto.protocol +
+      '//' +
+      goto.host +
+      origin.pathname +
+      origin.search +
+      origin.hash
+
+    chrome.tabs.update({url: newDestination})
+  })
+}
 
 
 var animateDots = function() {
-  var el = document.querySelector('#loading');
-  var dots = 0;
+  var el = document.querySelector('#loading')
+  var dots = 0
   setInterval(function() {
-    el.textContent = Array(dots++ % 5 + 1).join('.');
-  }, 400);
-};
+    el.textContent = Array(dots++ % 5 + 1).join('.')
+  }, 400)
+}
+
 
 document.addEventListener("submit", function(e) {
-  e.preventDefault();
-  var destination = document.querySelector('#domain').value;
+  e.preventDefault()
+  var destination = document.querySelector('#domain').value
 
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var key = makeKey(tabs[0].url);
-    var memory = load();
+    var key = makeKey(tabs[0].url)
+    var memory = load()
 
+    // if it's already something and is an array with length, ...
     if (memory[key] && memory[key].length) {
-      if (memory[key].indexOf(destination) === -1) {
-        memory[key].unshift(destination);
+      // ...then add it to the top.
+      var index = memory[key].indexOf(destination)
+      if (index > -1) {
+        // ...but remove it if it was already there.
+        memory[key].splice(index, 1)
       }
+      memory[key].unshift(destination)
     } else {
-      memory[key] = [destination];
+      memory[key] = [destination]
     }
-    save(memory);
-    submit(destination);
-  });
+    save(memory)
+    submit(destination)
+  })
 
-});
+})
 
 var load = function() {
-  return JSON.parse(localStorage.getItem('domainswitcher') || '{}');
-};
+  return JSON.parse(localStorage.getItem('domainswitcher') || '{}')
+}
 
 var save = function(stuff) {
-  localStorage.setItem('domainswitcher', JSON.stringify(stuff));
-};
+  localStorage.setItem('domainswitcher', JSON.stringify(stuff))
+}
 
 var makeKey = function(url) {
-  var parser = document.createElement('a');
-  parser.href = url;
-  return parser.protocol + parser.hostname;
-};
+  var parser = document.createElement('a')
+  parser.href = url
+  return parser.protocol + parser.hostname
+}
 
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  // console.log('Current tab:', tabs[0].url);
-  var memory = load();
-  var key = makeKey(tabs[0].url);
-  console.log("DURING LOADING", memory);
+  // console.log('Current tab:', tabs[0].url)
+  var memory = load()
+  var key = makeKey(tabs[0].url)
   if (memory[key]) {
-    document.querySelector('#domain').value = memory[key][0];
+    document.querySelector('#domain').value = memory[key][0]
 
-    var alternatives = document.querySelector('#alternatives');
+    var alternatives = document.querySelector('#alternatives')
     if (memory[key].length > 1) {
       memory[key].forEach(function(alternative, notfirst) {
         if (notfirst) {
-          var button = document.createElement('button');
-          button.classList.add('alternative');
-          button.textContent = alternative;
+          var button = document.createElement('button')
+          button.classList.add('alternative')
+          button.textContent = alternative
           button.addEventListener('click', function(event) {
-            event.preventDefault();
-            submit(alternative);
-          });
-          alternatives.appendChild(button);
+            event.preventDefault()
+            submit(alternative)
+          })
+          alternatives.appendChild(button)
         }
-      });
-      alternatives.style.display = 'block';
+      })
+      alternatives.style.display = 'block'
     } else {
-      alternatives.style.display = 'none';
+      alternatives.style.display = 'none'
     }
   }
 
-});
+})
---
+++ domainswitcher.js
@@ -0,0 +1,45 @@
+var urlparse = function(url) {
+  var parser = document.createElement('a')
+  parser.href = url
+  return parser
+}
+
+var matchedDomain = function(url) {
+  var domain = urlparse(url).host
+  // XXX make this config. Not everyone works on mozilla websites.
+  if (/mozilla\.(org|com)$/.test(domain)) {
+    return true
+  }
+  return false
+}
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (!changeInfo.url) {
+    return
+  }
+  if (!matchedDomain(changeInfo.url)) {
+    return
+  }
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.pageAction.show(tabId)
+  })
+})
+
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//   console.log('Current tab to start', tabs[0].url)
+// })
+
+// Happens when you click on a tab that was already loaded but not focused.
+// chrome.tabs.onActivated.addListener(function (activeInfo) {
+//   // console.log("Activated tab", activeInfo)
+//   // reset(activeInfo.tabId)
+// })
+
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  reset(tabs[0].id)
+})
+
+function reset(tabId) {
+  chrome.pageAction.hide(tabId)
+}
